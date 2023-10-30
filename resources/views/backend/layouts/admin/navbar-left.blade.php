7949992f972a5e452aa18a3eff1c00e81fc1d22c
P.PING <hathaichanok42@hotmail.com>
2023-10-30 10:51:48 +0700

    update css

--- resources/views/backend/layouts/admin/navbar-left.blade.php
+++ resources/views/backend/layouts/admin/navbar-left.blade.php
@@ -3,8 +3,8 @@
   <div class="sidenav-header">
     <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
     <a class="navbar-brand m-0" href="{{url('/dashboard')}}">
-      <img src="{{ asset('assets/img/logo-ct-dark.png')}}" class="navbar-brand-img h-100" alt="main_logo">
-      <span class="ms-1 font-weight-bold">ระบบหลังบ้าน</span>
+      {{-- <img src="{{ asset('assets/img/logo-ct-dark.png')}}" class="navbar-brand-img h-100" alt="main_logo"> --}}
+      <span class="ms-1 font-weight-bold text-copper" style="text-align: center; font-size:1.5rem;">THE SECRET</span>
     </a>
   </div>
   <hr class="horizontal dark mt-0">
@@ -13,7 +13,7 @@
       <li class="nav-item">
         <a class="nav-link" href="{{url('/dashboard')}}">
           <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-            <i class="fa fa-pie-chart text-info text-sm opacity-10 mb-2"></i>
+            <i class="fa fa-pie-chart text-copper text-sm opacity-10 mb-2"></i>
           </div>
           <span class="nav-link-text ms-1">ภาพรวม (Dashboard)</span>
         </a>
@@ -21,7 +21,7 @@
       <li class="nav-item">
         <a class="nav-link " href="{{url('/addpoint')}}">
           <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-            <i class="fa fa-plus-circle text-success text-sm opacity-10 mb-2"></i>
+            <i class="fa fa-plus-circle text-sm opacity-10 mb-2 text-dark"></i>
           </div>
           <span class="nav-link-text ms-1">เพิ่มพอยท์</span>
         </a>
@@ -29,7 +29,7 @@
       <li class="nav-item">
         <a class="nav-link " href="{{url('/member/list')}}">
           <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-            <i class="fa fa-users text-dark text-sm opacity-10 mb-2"></i>
+            <i class="fa fa-users text-copper text-sm opacity-10 mb-2"></i>
           </div>
           <span class="nav-link-text ms-1">สมาชิก</span>
         </a>
@@ -37,7 +37,7 @@
       <li class="nav-item">
         <a class="nav-link " href="{{url('/campaign')}}">
           <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-            <i class="fa fa-ticket text-danger text-sm opacity-10 mb-2"></i>
+            <i class="fa fa-ticket text-dark text-sm opacity-10 mb-2"></i>
           </div>
           <span class="nav-link-text ms-1">แคมเปญ</span>
         </a>
@@ -45,7 +45,7 @@
       <li class="nav-item">
         <a class="nav-link " href="{{url('/reward')}}">
           <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-            <i class="fa fa-gift text-info text-sm opacity-10 mb-2"></i>
+            <i class="fa fa-gift text-copper text-sm opacity-10 mb-2"></i>
           </div>
           <span class="nav-link-text ms-1">ของรางวัล</span>
         </a>
@@ -61,7 +61,7 @@
       <li class="nav-item">
         <a class="nav-link " href="{{url('/partner')}}">
           <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-            <i class="fa fa-handshake-o text-warning text-sm opacity-10 mb-2"></i>
+            <i class="fa fa-handshake-o text-copper text-sm opacity-10 mb-2"></i>
           </div>
           <span class="nav-link-text ms-1">เครือข่ายพันธมิตร</span>
         </a>
@@ -69,7 +69,7 @@
       <li class="nav-item">
         <a class="nav-link " href="{{url('/report-partner')}}">
           <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-            <i class="fa fa-file-pdf-o text-danger text-sm opacity-10 mb-2"></i>
+            <i class="fa fa-file-pdf-o text-dark text-sm opacity-10 mb-2"></i>
           </div>
           <span class="nav-link-text ms-1">รายงานข้อมูลร้านค้าพันธมิตร</span>
         </a>
@@ -77,7 +77,7 @@
       <li class="nav-item">
         <a class="nav-link " href="{{url('/article')}}">
           <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-            <i class="fa fa-newspaper-o text-dark text-sm opacity-10 mb-2"></i>
+            <i class="fa fa-newspaper-o text-copper text-sm opacity-10 mb-2"></i>
           </div>
           <span class="nav-link-text ms-1">บทความและข่าวสาร</span>
         </a>
@@ -85,7 +85,7 @@
       <li class="nav-item">
         <a class="nav-link " href="{{url('/media')}}">
           <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-            <i class="fa fa-image text-success text-sm opacity-10 mb-2"></i>
+            <i class="fa fa-image text-dark text-sm opacity-10 mb-2"></i>
           </div>
           <span class="nav-link-text ms-1">Media</span>
         </a>
@@ -94,7 +94,7 @@
         <a class="nav-link" href="{{ route('admin.logout') }}" onclick="event.preventDefault();
         document.getElementById('logout-form').submit();">
           <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-            <i class="ni ni-button-power text-dark text-sm opacity-10 mb-1"></i>
+            <i class="ni ni-button-power text-copper text-sm opacity-10 mb-1"></i>
           </div>
           <span class="nav-link-text ms-1">ออกจากระบบ</span>
         </a>
